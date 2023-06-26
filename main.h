1e69cbb9104eb7a35560e82898e403497b05072d
ahmed eid <ahmedeidgoma333@gmail.com>
2023-06-25 18:00:22 -0700

    v2.1

--- main.h
+++ main.h
@@ -5,12 +5,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-
 int _printf(const char *format, ...);
 char *from_int_to_string(int value);
 void str_concat(int pos, char *str1, char *str2);
 int _strlen(const char *str);
-int get_size(int len,const char *format, va_list args);
-void fill_buf(int size, const char* format, va_list args, char* buf);
+int get_size(int len, const char *format, va_list args);
+void fill_buf(int size, const char *format, va_list args, char *buf);
 
 #endif /*MAIN_H*/
