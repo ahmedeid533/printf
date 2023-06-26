2bb4d6c22950413a87afa653448eafc54ad74d35
ahmed eid <ahmedeidgoma333@gmail.com>
2023-06-25 17:28:24 -0700

    v2.0

--- function.c
+++ function.c
@@ -20,7 +20,7 @@ int get_size(int len, const char *format, va_list args)
 		if (format[i] == '%')
 		{
 			i++;
-			if (format[i] == '%')
+			if (format[i] == '%' || format[i] == 'c')
 			{
 				size++;
 			}
@@ -29,6 +29,10 @@ int get_size(int len, const char *format, va_list args)
 				value = va_arg(args, int);
 				str = from_int_to_string(value);
 				size += _strlen(str);
+			}
+			else if (format[i] == 's')
+			{
+				size += _strlen(va_arg(args, char*));
 			}
 			else
 				size += 2;
@@ -58,12 +62,7 @@ void fill_buf(int size, const char *format, va_list args, char *buf)
 		if (format[j] == '%')
 		{
 			j++;
-			if (format[j] == '%')
-			{
-				j++;
-				buf[i] = '%';
-			}
-			else if (format[j] == 'd' || format[j] == 'i')
+			if (format[j] == 'd' || format[j] == 'i')
 			{
 				value = va_arg(args, int);
 				str = from_int_to_string(value);
@@ -71,8 +70,22 @@ void fill_buf(int size, const char *format, va_list args, char *buf)
 				i += _strlen(str) - 1;
 				j++;
 			}
+			else if (format[j] == 's')
+			{
+				str = va_arg(args, char*);
+				str_concat(i, buf, str);
+				i += _strlen(str) - 1;
+			}
+			else if (format[j] == 'c')
+				buf[i] = va_arg(args, int);
 			else
+			{
 				buf[i] = '%';
+				j--;
+			}
+			j++;
+			if (format[j] == '%')
+				j++;
 		}
 		else
 		{
